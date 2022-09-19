61c6be19ccfc4f13ddccaea1856e6d9634c3c644
ogzhn <oguzhanozdemir.qa@gmail.com>
2022-09-19 13:24:11 +0300

    ders icerikleri eklendi

---
+++ conditionals/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 14;
+        if (sayi < 20) {
+            System.out.println("Sayı 20'den küçüktür");
+        } else if (sayi == 20) {
+            System.out.println("Sayı 20'ye eşittir");
+        }
+    }
+}
---
+++ variables/variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ogreciSayisi = 11;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogreciSayisi);
+        System.out.println("Öğrenci sayım : "+ogreciSayisi);
+
+    }
+}
